702f4da2e672974fed426077444411aaabaaabed
Frank Ruiz <frankruiz49@gmail.com>
2019-08-27 19:59:33 -0700

    styled forms for new article and edited article

--- Alpha_Blog/app/assets/javascripts/application.js
+++ Alpha_Blog/app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 $(function(){ $(document).foundation(); });
 //= require turbolinks
 //= require_tree .
+$(function () {
+  var showClass = 'show';
+
+  $('input').on('checkval', function () {
+    var label = $(this).prev('label');
+    if(this.value !== '') {
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  }).on('keyup', function () {
+    $(this).trigger('checkval');
+  });
+});
+

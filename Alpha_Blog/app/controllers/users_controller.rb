ecb16863adfc6b1f18a5c4030b5d37dc0a267837
Frank Ruiz <frankruiz49@gmail.com>
2019-08-29 22:15:50 -0700

    signup page is working/displays save and errors

--- Alpha_Blog/app/controllers/users_controller.rb
+++ Alpha_Blog/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController <  ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        flas[:success] = "Welcome to the alpha blog #{@user.username}"
+        flash[:notice] = "Welcome to the alpha blog #{@user.username}"
         redirect_to articles_path
 
     else 
